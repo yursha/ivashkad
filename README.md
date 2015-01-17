f934ce78f62716514e8e7578b3245b2b4ffdb708
Alex Yursha <alexyursha@gmail.com>
2015-01-17 04:33:47 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ivashkad
-Ethernet command-line chat server. Designed to work with Ivashka client.
+Ethernet command-line chat server. Designed to work with [ivashka](https://github.com/yursha/ivashka) client.
+
+# Installation
+
+`npm install -g ivashkad`
+
+# Usage
+
+`ivashkad`
+
+Listens on port 6666 which is the standard port for *ivashka* server.
---
+++ index.js
@@ -0,0 +1,94 @@
+var net = require('net');
+
+/*
+ * Port to listen to.
+ */
+var port = 6666;
+
+/*
+ * Name of the speaking chat robot
+ */
+var bot = 'Ivashka';
+
+var connectionsCount = 0;
+var users = {};
+
+var server = net.createServer(function (conn) {
+
+  // executes every time a new connection to the server is established.
+  var nickname;
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', function (data) {
+    data = data.replace(/[\n\r]/g, '') // strip the "new line" character
+    if (!nickname) {
+      if (users[data]) {
+        var message = {
+          author: bot,
+          text: 'nickname already in use. try again: '
+        };
+        unicast(message, conn);
+        return;
+      } else {
+        nickname = data;
+        users[nickname] = conn;
+        var message = {
+          author: bot,
+          text: nickname + ' joined the room'
+        };
+        broadcast(message, conn);
+        console.log('\033[90m > ' + nickname + ' joined the room\033[39m');
+      }
+    } else {
+
+      // a chat message
+      var message = {
+        author: nickname,
+        text: data
+      };
+      broadcast(message, true);
+    }
+  });
+
+  conn.on('close', function () {
+    connectionsCount--;
+    delete users[nickname];
+    var message = {
+      author: bot,
+      text: nickname + ' left the room'
+    };
+    broadcast(message);
+    console.log('\033[90m > ' + nickname + ' left the room\033[39m');
+  });
+
+  unicast({
+    author: 'Welcome',
+    text: connectionsCount
+  }, conn);
+
+  connectionsCount++;
+
+  function broadcast(msg, exceptMyself) {
+    for (var i in users) {
+      if (!exceptMyself || i != nickname) {
+        users[i].write(JSON.stringify(msg));
+      }
+    }
+  }
+
+});
+
+function unicast(msg, user) {
+  user.write(JSON.stringify(msg));
+}
+
+
+server.listen(port, function () {
+  console.log('\033[96m    server listening on *:' + port + '\033[39m');
+});
+
+// TODO: let it be killed with C-c
+//server.on('SIGINT', function () {
+//  process.exit(0);
+//})
